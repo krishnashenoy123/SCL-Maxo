da50717f3030be33f513b4d876b558054b9f8d12
RishabhNaik <rishabhnaik37@gmail.com>
2020-12-03 16:15:00 +0530

    Added all the files for development

---
+++ Project/focus/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home,name='focus-home' ),
+    path('about/', views.about,name='focus-about'),
+    path('login/', views.login,name='focus-login'),
+    path('register/', views.register,name='focus-register')
+]
