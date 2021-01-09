274add32e5e4ba83ed56af74f72b72a0b48c493c
RishabhNaik <rishabhnaik37@gmail.com>
2021-01-09 16:18:59 +0530

    Major update on creating rooms

--- Project/focus/urls.py
+++ Project/focus/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    # PostUploadView
+)
 from . import views
 
 urlpatterns = [
     path('', views.home,name='focus-home' ),
-    path('about/', views.about,name='focus-about')
-    # path('register/', views.register,name='focus-register')
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    # path('post/<int:pk>/upload/', PostUploadView.as_view(), name='post-upload'),
+    path('about/', views.about, name='focus-about')
 ]
--- Project/focus/views.py
+++ Project/focus/views.py
@@ -1,34 +1,101 @@
 from django.shortcuts import render
-# Create your views here.
-posts= [
-    {
-        'author':'Rishabh',
-        'title':'blog-post1',
-        'content':'First post',
-        'date_posted':'August 27, 2018'
-    },
-    {
-        'author':'Hrishi',
-        'title':'blog-post2',
-        'content':'Second post',
-        'date_posted':'August 29, 2018'
-    }
-
-]
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Post
+# from .models import Files
+import pyrebase
 
 
 def home(request):
     context = {
-        'posts':posts
+        'posts': Post.objects.all()
     }
-    return render(request, 'focus/home.html',context)
+    return render(request, 'focus/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'focus/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
-    return render(request, 'focus/about.html',{'title':'About'})
+    return render(request, 'focus/about.html', {'title': 'About'})
 
+# class PostUploadView(DetailView):
+#     model = Files
+#     fields = ['names', 'content']
 
+#     def test_func(self):
+#         post = self.get_object()
+#         if self.request.user == post.author:
+#             return True
+#         return False
 
-
+#     config={
+#             'apiKey': "AIzaSyDLzBjNqg37fRiCBClXzamhjdVN2jr4nX8",
+#             'authDomain': "fir-9f329.firebaseapp.com",
+#             'databaseURL':"https://fir-9f329.firebaseio.com",
+#             'projectId': "fir-9f329",
+#             'storageBucket': "fir-9f329.appspot.com",
+#             'messagingSenderId': "1022042387286",
+#             'appId': "1:1022042387286:web:efbafb88c136023dd0dcf7",
+#             'measurementId': "G-F8NR0833P8"
+#             }
+#     firebase= pyrebase.initialize_app(config)
+#     storage=firebase.storage()
+#     path_local= names
+    
+#     path_on_cloud = post.id+'/'
+#     storage.child(path_on_cloud).put(path_local)
+#     Url=storage.child(path_on_cloud).get_url(None)
+#     print(Url)
 
 
 
